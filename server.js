a2e386aa5cd478be3ff95ab8f2b78fc07848e4c1
Douglas Snodgrass <snodgrassdouglas@gmail.com>
2022-07-19 16:41:12 -0700

    cant start webserver

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
-const apiRoutes = require('./Develop/routes/apiRoutes');
-const htmlRoutes = require('./Develop/routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.urlencoded({
-    extended: true
-}));
-
-app.use('/',  htmlRoutes);
-app.use(express.json());
 app.use(express.static('public'));
-app.use('/api',apiRoutes);
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data
+app.use(express.json());
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+    console.log(`API server now on port ${PORT}. Welcome!`);
+  });
