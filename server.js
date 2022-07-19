cbeecc0e03ce45735b087f32c1cdf275ebf015a8
Douglas Snodgrass <snodgrassdouglas@gmail.com>
2022-07-19 14:27:49 -0700

    moved routes to dev

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 3000;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
 
 app.use(express.urlencoded({
     extended: true
